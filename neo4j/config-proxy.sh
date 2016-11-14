1b4a4768de481e107ae760b806d0650675e073a1
mkoertgen <marcel.koertgen@gmail.com>
2016-11-14 16:13:06 +0100

    fixed apoc proxy issue

--- neo4j/config-proxy.sh
+++ neo4j/config-proxy.sh
@@ -10,8 +10,7 @@ if [[ ${http_proxy:+1} ]]; then
 	proxyHost=${proxyHost//:*} 
 	proxyPort=${proxyPort//"/"} 
 	# note: assuming same for http/s
-	# NOTE:neo4j does not consider JAVA_OPTS but we can inject JAVA_CMD, cf.
-	# https://github.com/neo4j/neo4j/blob/124d6ac1d3de96738aa62afb68832781d2b52d8a/packaging/standalone/src/main/distribution/shell-scripts/bin/neo4j#L113
-	# https://github.com/neo4j/neo4j/blob/124d6ac1d3de96738aa62afb68832781d2b52d8a/packaging/standalone/src/main/distribution/shell-scripts/bin/neo4j-shared.sh#L90
-	export JAVA_OPTS="-Dhttp.proxyHost=${proxyHost} -Dhttp.proxyPort=${proxyPort} -Dhttps.proxyHost=${proxyHost} -Dhttps.proxyPort=${proxyPort} -Dhttp.nonProxyHosts=$no_proxy -Dhttps.nonProxyHosts=$no_proxy"
+	# NOTE:neo4j does not consider JAVA_OPTS but we can use 'dbms_jvm_additional', cf.
+	# https://github.com/neo4j-contrib/neo4j-apoc-procedures/issues/207#issuecomment-260083568
+	export dbms_jvm_additional="-Dhttp.proxyHost=${proxyHost} -Dhttp.proxyPort=${proxyPort} -Dhttps.proxyHost=${proxyHost} -Dhttps.proxyPort=${proxyPort} -Dhttp.nonProxyHosts=$no_proxy -Dhttps.nonProxyHosts=$no_proxy"
 fi 
