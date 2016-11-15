97b0f26e80fc4e5ab81cf92db5485e3c2addfd27
mkoertgen <marcel.koertgen@gmail.com>
2016-11-15 13:37:07 +0100

    logstash proxy & log issues

---
+++ logstash/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+source /config-proxy.sh
+
+# Add logstash as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- logstash "$@"
+fi
+
+# Run as user "logstash" if the command is "logstash"
+if [ "$1" = 'logstash' ]; then
+	set -- gosu logstash "$@"
+fi
+
+exec "$@"
---
+++ logstash/install-plugins.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+source /config-proxy.sh
+
+# Install plugins here
