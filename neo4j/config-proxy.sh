2cc0ee24a8df46679b6051ea73d678d8f169110d
mkoertgen <marcel.koertgen@gmail.com>
2016-11-15 12:12:38 +0100

    Simplified neo4j apoc installation & considered proxy issues

---
+++ neo4j/config-proxy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if [[ ${http_proxy:+1} ]]; then 
+	echo
+	echo "Configure proxy '${http_proxy}' ..."
+	proxyHost=${http_proxy//"http://"} 
+	proxyPort=${proxyHost##*:} 
+	proxyHost=${proxyHost//:*} 
+	proxyPort=${proxyPort//"/"} 
+	# note: assuming same for http/s
+	# NOTE:neo4j does not consider JAVA_OPTS but we can use 'dbms_jvm_additional', cf.
+	# https://github.com/neo4j-contrib/neo4j-apoc-procedures/issues/207#issuecomment-260083568
+	export dbms_jvm_additional="-Dhttp.proxyHost=${proxyHost} -Dhttp.proxyPort=${proxyPort} -Dhttps.proxyHost=${proxyHost} -Dhttps.proxyPort=${proxyPort} -Dhttp.nonProxyHosts=$no_proxy -Dhttps.nonProxyHosts=$no_proxy"
+fi 
---
+++ neo4j/docker-entrypoint2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+source /config-proxy.sh
+
+source /docker-entrypoint.sh
