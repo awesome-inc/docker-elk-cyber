034bbb53b50ee48c4477df3dac3ded9aa3871fb2
mkoertgen <marcel.koertgen@gmail.com>
2018-03-24 09:08:43 +0100

    Simplify repo

--- elasticsearch/install-plugins.sh
+++ elasticsearch/install-plugins.sh
@@ -3,10 +3,10 @@ set -e
 
 source ./config-proxy.sh
 
-#bin/elasticsearch-plugin install ingest-attachment
+bin/elasticsearch-plugin install ingest-attachment
 
-nlp_version=${ELASTICSEARCH_VERSION}.1
+nlp_version=${ELK_VERSION}.1
 nlp_file=ingest-opennlp-${nlp_version}.zip
 nlp_url=https://github.com/spinscale/elasticsearch-ingest-opennlp/releases/download/${nlp_version}/${nlp_file}
 if [ -e "${PWD}/${nlp_file}" ]; then nlp_url=file://${PWD}/${nlp_file}; fi
-bin/elasticsearch-plugin install ${nlp_url} 
+bin/elasticsearch-plugin install ${nlp_url}
