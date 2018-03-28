cc67aecb8af0a6fc1fa67a5c79574d03c5f41980
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-03-28 18:10:27 +0200

    (provisioner) Cleanup wait for ES

--- provisioner/00_wait_for_es.rb
+++ provisioner/00_wait_for_es.rb
@@ -2,36 +2,51 @@ require 'json'
 require 'net/http'
 require 'uri'
 
-HEADER = { 'Content-Type' => 'application/json' }.freeze
-INTERVAL = 5
-VALID_STATUS = %w[yellow green].freeze
-
-def es_base_uri
-  ENV['ES_BASE_URI'] || 'http://localhost:9200'
-end
-
-def cluster_status
-  uri = URI.parse "#{es_base_uri}/_cluster/health"
-  http = Net::HTTP.new(uri.host, uri.port)
-  request = Net::HTTP::Get.new(uri.request_uri, HEADER)
-
-  begin
-    response = http.request(request)
-  rescue Errno::ECONNREFUSED
-    return 'red'
+# Wait for Elasticsearch
+class WaitForEs
+  def self.call(uri)
+    WaitForEs.new(uri).wait
   end
 
-  return 'red' unless response.code.to_i == 200
+  def initialize(uri)
+    safe_uri = uri || ES_BASE_URI
+    @uri = URI.parse("#{safe_uri}/_cluster/health")
+  end
 
-  result = JSON.parse(response.body)
-  result['status']
+  def wait
+    status = cluster_status
+    puts 'Waiting for Elasticsearch to come up...'
+    until VALID_STATUS.include? status
+      puts "Waiting #{INTERVAL} seconds..."
+      sleep INTERVAL
+      status = cluster_status
+      puts "Status: #{status}."
+    end
+  end
+
+  private
+
+  attr_reader :uri
+
+  ES_BASE_URI = ENV.fetch('ES_BASE_URI') { 'http://localhost:9200' }.freeze
+  HEADER = { 'Content-Type' => 'application/json' }.freeze
+  INTERVAL = 5
+  VALID_STATUS = %w[yellow green].freeze
+
+  def cluster_status
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri, HEADER)
+    begin
+      response = http.request(request)
+    rescue Errno::ECONNREFUSED
+      return 'red'
+    end
+
+    return 'red' unless response.code.to_i == 200
+
+    result = JSON.parse(response.body)
+    result['status']
+  end
 end
 
-status = cluster_status
-puts 'Waiting for Elasticsearch to come up...'
-until VALID_STATUS.include? status
-  puts "Waiting #{INTERVAL} seconds..."
-  sleep INTERVAL
-  status = cluster_status
-  puts "Status: #{status}."
-end
+WaitForEs.call ARGV[0]
